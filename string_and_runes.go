f246970f38b546576f4ed4d2a21d58359cb81eae
mts0629 <soratobi93@gmail.com>
2024-10-22 01:19:23 +0900

    More on runes

--- string_and_runes.go
+++ string_and_runes.go
@@ -1,22 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "unicode/utf8"
+)
 
 func main() {
-	s := "あいうえお"
-
+	s := "ABCあいうえお" // string: immutable array of UTF-8 code points in byte
 	fmt.Println(s)
 
-	fmt.Println("Byte expression")
+	fmt.Println("Byte expression") // byte: 8-bit integer
 	for i := 0; i < len(s); i++ {
 		fmt.Printf("%x ", s[i])
 	}
 	fmt.Println()
-	fmt.Println("Length:", len(s))
+	fmt.Println("Length (byte):", len(s))
 
-	fmt.Println("Unicode expression")
+	fmt.Println("Rune expression") // rune: 32-bit integer
 	for _, runeVal := range s {
-		fmt.Printf("%#U ", runeVal)
+		fmt.Printf("%c (%#U) ", runeVal, runeVal)
 	}
 	fmt.Println()
+	fmt.Println("Length (rune):", len([]rune(s)))
+	// fmt.Println("Length (rune):", utf8.RuneCountInString(s)) // Count in runes
 }
