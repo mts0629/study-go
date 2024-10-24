7236069ae42fb882471a47bf09351cbbab98c42b
mts0629 <soratobi93@gmail.com>
2024-10-24 23:34:46 +0900

    Read from a text file

--- file_io.go
+++ file_io.go
@@ -5,23 +5,53 @@ import (
 	"os"
 )
 
-func main() {
+func write_file() {
+	// Create/open a file
 	f, err := os.Create("./work/test.txt")
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Failed to create file")
 	}
 
+	defer f.Close() // Defer the execution of Close()
+
 	str := "Hello, world!\n"
 	data := []byte(str)
 
+	// Write bytes to a file
 	count, err := f.Write(data)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Failed to write file")
 	}
 
-	fmt.Printf("Write %d bytes\n", count)
+	fmt.Printf("Write %d bytes to \"%s\"\n", count, f.Name())
+}
+
+func read_file() {
+	// Open a file
+	f, err := os.Open("./work/test.txt")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Failed to open file")
+	}
 
 	defer f.Close()
+
+	// Read from the file
+	b := make([]byte, 16) // Allocate buffer
+	n, err := f.Read(b)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Failed to read file")
+	}
+
+	fmt.Printf("Read %d bytes from \"%s\"\n", n, f.Name())
+	fmt.Printf("%s\n", string(b[:n]))
+}
+
+func main() {
+	write_file()
+
+	read_file()
 }
