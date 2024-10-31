b55005d0d0da9f2dea7da0c4620342f883a8b0e5
mts0629 <soratobi93@gmail.com>
2024-10-31 23:57:53 +0900

    Sync with WaitGroup

--- goroutine.go
+++ goroutine.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func f(msg string) {
+// Worker function
+func worker(msg string, wg *sync.WaitGroup) {
+	defer wg.Done() // Notify this the worker is done on the last
+
 	for i := 0; i < 5; i++ {
 		fmt.Println(msg, ":", i)
+		time.Sleep(time.Millisecond)
 	}
 }
 
 func main() {
-	// Goroutine, lightweight thread
-	go f("Goroutine #1")
-	go f("Goroutine #2")
+	// WaitGroup for synchronization of goroutines
+	var wg sync.WaitGroup
 
-	time.Sleep(time.Second)
+	// Goroutine, lightweight thread
+	wg.Add(1)
+	go worker("Goroutine #1", &wg)
+	wg.Add(1)
+	go worker("Goroutine #2", &wg)
+
+	// Wait finish of goroutines
+	wg.Wait()
 }
