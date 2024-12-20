0e7097b7480958d4bec15af27f4539d13676ade1
mts0629 <soratobi93@gmail.com>
2024-12-20 09:29:37 +0900

    Non-blocking channel operations

---
+++ go_by_example/non_blocking_channel_ops.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	// Non-blocking receive
+	default:
+		fmt.Println("no message received")
+	}
+
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	// Non-blocking send
+	default:
+		fmt.Println("no message sent")
+	}
+
+	// Multi-way non-blocking select
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
