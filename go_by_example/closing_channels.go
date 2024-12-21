7164c0634c88a1352967e2556c6e2b6bd8349145
mts0629 <soratobi93@gmail.com>
2024-12-21 23:59:31 +0900

    Closing channels

---
+++ go_by_example/closing_channels.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	// Job which close when all values have been received
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	// Send 3 jobs and close
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
+
+	_, ok := <-jobs
+	fmt.Println("received more jobs:", ok)
+}
