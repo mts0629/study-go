551e7f5b0e27925fcbfbdf4d1555eae4e9d0b054
mts0629 <soratobi93@gmail.com>
2025-03-18 23:28:59 +0900

    String functions

---
+++ go_by_example/string_functions.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+// Alias
+var p = fmt.Println
+
+func main() {
+	// String utilities
+	p("Contains:  ", s.Contains("test", "s"))
+	p("Count:     ", s.Count("test", "t"))
+	p("HasPrefix: ", s.HasPrefix("test", "te"))
+	p("HasSuffix: ", s.HasSuffix("test", "st"))
+	p("Index:     ", s.Index("test", "e"))
+	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
+	p("Repeat:    ", s.Repeat("a", 5))
+	p("Replace:   ", s.Replace("foo", "o", "0", -1))
+	p("Replace:   ", s.Replace("foo", "o", "0", 0))
+	p("Split:     ", s.Split("a-b-c-d-e", "-"))
+	p("ToLower:   ", s.ToLower("TEST"))
+	p("ToUpper:   ", s.ToUpper("test"))
+}
