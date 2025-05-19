9d97bd55c84058aa503dc182bce0b4d9fbf68e48
mts0629 <soratobi93@gmail.com>
2025-05-19 23:38:25 +0900

    Map I/O

--- tutorials/greetings/greetings.go
+++ tutorials/greetings/greetings.go
@@ -6,7 +6,7 @@ import (
 	"math/rand"
 )
 
-// Hello returns a greeting for the named person
+// Return a greeting for the named person
 func Hello(name string) (string, error) {
 	// If no name was given, return an error with a message
 	if name == "" {
@@ -17,6 +17,24 @@ func Hello(name string) (string, error) {
 	// message := fmt.Sprintf(randomFormat(), name)
 	message := fmt.Sprintf(randomFormat(), "") // Cause an error
 	return message, nil
+}
+
+// Return a map that associates each of the named people
+// with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages
+	messages := make(map[string]string)
+	// Loop through the received slice of names,
+	// calling the Hello function to get a message for each name
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with the name
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // Return one of a set of greeting messages
